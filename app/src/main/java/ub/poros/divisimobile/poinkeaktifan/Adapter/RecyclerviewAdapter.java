fe435b372c457cfb66d3f0cdecd982867e2414b6
Abidin <abidinadip@gmail.com>
2018-09-09 13:56:28 +0700

    Add: library support design, recylerview

---
+++ app/src/main/java/ub/poros/divisimobile/poinkeaktifan/Adapter/RecyclerviewAdapter.java
@@ -0,0 +1,4 @@
+package ub.poros.divisimobile.poinkeaktifan.Adapter;
+
+public class RecyclerviewAdapter {
+}
---
+++ app/src/main/java/ub/poros/divisimobile/poinkeaktifan/Model/model.java
@@ -0,0 +1,4 @@
+package ub.poros.divisimobile.poinkeaktifan.Model;
+
+public class model {
+}
